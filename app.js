bb64e7eff32a57b0754e48133f5cbd9afe6cd002
JonatSrG <JonatSrG>
2022-10-19 13:51:44 -0500

    manejo de errores e insercion de registros

--- app.js
+++ app.js
@@ -12,9 +12,53 @@ request.onupgradeneeded = event => {
     db.createObjectStore('heroes', {
 
         keyPath: 'id'
-        
+
     });
 }
+
+//manejo de errores
+request.onError = event => {
+
+    console.log('DB error: ', event.target.error );
+
+}
+
+//Insertar Datos
+request.onsuccess = event => {
+
+    let db = event.target.result;
+
+    let heroesData = [
+        { id: '1111', heroe: 'Spiderman', mensaje: 'Aqui su amigo Spiderman'},
+        { id: '2222', heroe: 'Iroman', mensaje: 'Aqui en mi nuevo Traje'},
+    ];
+
+    let heroesTransaction = db.transaction('heroes', 'readwrite');
+
+    heroesTransaction.onError = event => {
+        console.log('Error guardando', event.target.error );
+    };
+
+    //Informa del exito de la transaccion
+    heroesTransaction.oncomplete = event => {
+        
+        console.log('Transaccion hecha', event );
+
+    };
+    //objeto
+    let heroesStore = heroesTransaction.objectStore('heroes');
+
+    for (let heroe of heroesData) {
+
+        heroesStore.add( heroe );
+
+    }
+
+    heroesStore.onsuccess = event => {
+        console.log('Nuevo item agregado a la base de datos');
+    };
+}
+
 
 
 
