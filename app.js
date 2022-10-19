3b6be7c84b3e7f9c620af43eb4a00e30f737803f
JonatSrG <JonatSrG>
2022-10-19 13:35:58 -0500

    Inicios de Indexed DB

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 
 // indexedDB: Reforzamiento
+let request = window.indexedDB.open('mi-database', 1);
 
+//Se actualiza cuando se crea o se sube de version de la BD
+request.onupgradeneeded = event => {
+
+    console.log('Actualizacion de BD');
+
+    let db = event.target.result;
+
+    db.createObjectStore('heroes', {
+
+        keyPath: 'id'
+        
+    });
+}
 
 
 
